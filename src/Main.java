1b45a23a246c1924cc2eb997563d1016353fa452
freshskates <business.robertcacho@gmail.com>
2021-06-01 14:41:58 -0700

    Add JavaDoc

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     /**
      * Main class only uses the Panel Class,
      * Panel Class has access to Data and Handles it accordingly
-     * @see Panel Program Secondary Entry Point
+     * @see Panel acts as the Secondary Entry Point
      */
     public static void main(String[] args) {
         Panel panel = new Panel();
