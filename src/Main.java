5f02d4dfb79e4f8c9e255cb4befb1a181d727730
freshskates <business.robertcacho@gmail.com>
2021-06-10 17:16:40 -0700

    Add JavaDoc

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,12 @@ import panel.Panel;
 
 public class Main {
     /**
+     * @author Robert Cacho Ruiz
+     *
      * Main class only uses the Panel Class,
      * Panel Class has access to Data and Handles it accordingly
-     * @see Panel acts as the Secondary Entry Point
+     * @see builder.Data for all Data used
+     * @see Panel : acts as the Secondary Entry Point
      */
     public static void main(String[] args) {
         Panel panel = new Panel();
